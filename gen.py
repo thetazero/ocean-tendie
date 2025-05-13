7b5325258e8e7ddd3d670104bf5078191c596276
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2025-05-12 21:58:45 -0400

    add event list

--- gen.py
+++ gen.py
@@ -1,6 +1,19 @@
 from typing import NamedTuple
+from datetime import datetime, timedelta
+from dataclasses import dataclass
 
-class Event(NamedTuple):
+
+
+DOUBLE_BACKSLASH = "\\\\"
+NEWLINE = "\n"
+
+@dataclass
+class EventDef:
+    name: str
+    duration: int # In minutes
+
+@dataclass
+class Event:
     name: str
     time: str
 
@@ -9,17 +22,47 @@ def gen_event_list(events: list[Event]) -> str:
 
 \\begin{{tabular}}{{@{{}}lllll@{{}}}}
 \\toprule
-\\textbf{{Event \\#}} & \\textbf{{Event Name}} & \\textbf{{Gender}} & \\textbf{{Division}} & \\textbf{{Time}} \\\\
+\\textbf{{Event \\#}} & \\textbf{{Event Name}} &  \\textbf{{Time}} \\\\
 \\midrule
-1 & 100m Dash       & M/F & JV/V & \\\\
-2 & 4x100m Relay    & M/F & JV/V & \\\\
-3 & 1600m Run       & M/F & JV/V & \\\\
-... & ...           & ... & ...  & \\\\
+{
+    (NEWLINE).join(
+        f"{i + 1} & {event.name} & {event.time} {DOUBLE_BACKSLASH}"
+        for i, event in enumerate(events)
+    )
+}
 \\bottomrule
 \\end{{tabular}}
 """
 
-def generate_heat_sheet(name: str, date: str, location: str, host: str, events: list[Event]) -> str:
+def render_event_heat(event: Event) -> str:
+    return f"""
+\\textbf{{Event:}} {event.name} \\quad \\textbf{{Time:}} {event.time} 
+
+\\vspace{{1em}}
+\\begin{{tabular}}{{@{{}}lllll@{{}}}}
+\\toprule
+\\textbf{{Lane}} & \\textbf{{Athlete Name}} & \\textbf{{School/Team}} & \\textbf{{Seed Time}} \\\\
+\\midrule
+1 & & & \\\\
+2 & & & \\\\
+3 & & & \\\\
+4 & & & \\\\
+5 & & & \\\\
+6 & & & \\\\
+7 & & & \\\\
+8 & & & \\\\
+\\bottomrule
+\\end{{tabular}}
+\\vspace{{2.5em}}
+"""
+
+def generate_heat_sheet(name: str, date: str, location: str, host: str, events: list[EventDef], start_time: datetime, between_event_time: timedelta) -> str:
+    proc_events: list[Event] = []
+    for event in events:
+        time_delta = timedelta(minutes=event.duration)
+        proc_events.append(Event(name=event.name, time=start_time.strftime("%-I:%M %p")))
+        start_time += time_delta + between_event_time
+
     return f"""\\documentclass[11pt]{{article}}
 \\usepackage[margin=1in]{{geometry}}
 \\usepackage{{booktabs}}
@@ -31,6 +74,7 @@ def generate_heat_sheet(name: str, date: str, location: str, host: str, events: 
 \\titleformat{{\\subsection}}{{\\normalsize\\bfseries}}{{}}{{0em}}{{}}
 
 \\setlist[itemize]{{noitemsep, topsep=0pt}}
+\\setlength{{\\parindent}}{{0pt}}
 
 \\begin{{document}}
 
@@ -44,54 +88,19 @@ def generate_heat_sheet(name: str, date: str, location: str, host: str, events: 
 
 \\vspace{{1em}}
 
-{gen_event_list(events)}
+{gen_event_list(proc_events)}
 
 
 \\vspace{{2em}}
 
 \\section*{{Track Event Heat Sheet}}
 
-\\textbf{{Event:}} \\rule{{10cm}}{{0.4pt}} \\\\
-\\textbf{{Gender:}} \\rule{{3cm}}{{0.4pt}} \\hspace{{1cm}} \\textbf{{Division:}} \\rule{{3cm}}{{0.4pt}} \\hspace{{1cm}} \\textbf{{Time:}} \\rule{{3cm}}{{0.4pt}}
-
-\\vspace{{1em}}
-
-\\begin{{tabular}}{{@{{}}lllll@{{}}}}
-\\toprule
-\\textbf{{Heat}} & \\textbf{{Lane}} & \\textbf{{Athlete Name}} & \\textbf{{School/Team}} & \\textbf{{Seed Time}} \\\\
-\\midrule
-1 & 1 & & & \\\\
-  & 2 & & & \\\\
-  & 3 & & & \\\\
-  & 4 & & & \\\\
-  & 5 & & & \\\\
-  & 6 & & & \\\\
-  & 7 & & & \\\\
-  & 8 & & & \\\\
-2 & 1 & & & \\\\
-... & ... & ... & ... & ... \\\\
-\\bottomrule
-\\end{{tabular}}
-
-\\vspace{{2em}}
-
-\\section*{{Field Event Flight Sheet}}
-
-\\textbf{{Event:}} \\rule{{10cm}}{{0.4pt}} \\\\
-\\textbf{{Gender:}} \\rule{{3cm}}{{0.4pt}} \\hspace{{1cm}} \\textbf{{Division:}} \\rule{{3cm}}{{0.4pt}} \\hspace{{1cm}} \\textbf{{Start Time:}} \\rule{{3cm}}{{0.4pt}}
-
-\\vspace{{1em}}
-
-\\begin{{tabular}}{{@{{}}lllll@{{}}}}
-\\toprule
-\\textbf{{Flight}} & \\textbf{{Order}} & \\textbf{{Athlete Name}} & \\textbf{{School/Team}} & \\textbf{{Seed Mark}} \\\\
-\\midrule
-1 & 1 & & & \\\\
-  & 2 & & & \\\\
-  & 3 & & & \\\\
-... & ... & ... & ... & ... \\\\
-\\bottomrule
-\\end{{tabular}}
+{
+    (NEWLINE).join(
+        render_event_heat(event)
+        for event in proc_events
+    )
+}
 
 \\end{{document}}
 """
@@ -99,25 +108,27 @@ def generate_heat_sheet(name: str, date: str, location: str, host: str, events: 
 
 if __name__ == "__main__":
     events = [
-        Event(name="Blind Walk", time="TBD"),
-        Event(name="Boot 100", time="TBD"),
-        Event(name="100m Relay", time="TBD"),
-        Event(name="Random 400-2000", time="TBD"),
-        Event(name="100 and 10 Hurdles", time="TBD"),
-        Event(name="1k Std Dev", time="TBD"),
-        Event(name="Shot Relay", time="TBD"),
-        Event(name="Quadruple Jump", time="TBD"),
-        Event(name="Wheel Throw", time="TBD"),
-        Event(name="Frisbee Put", time="TBD"),
+        EventDef(name="100 and 10 Hurdles", duration=7),
+        EventDef(name="Blind Walk", duration=7),
+        EventDef(name="Random 400-2000", duration=10),
+        EventDef(name="Wheel Throw", duration=8),
+        EventDef(name="Frisbee Put", duration=15),
+        EventDef(name="Shot Relay", duration=9),
+        EventDef(name="100m Relay", duration=5),
+        EventDef(name="Quadruple Jump", duration=12),
+        EventDef(name="Boot 100m", duration=5),
+        EventDef(name="1k Std Dev", duration=8),
     ]
     with open("heat_sheet.tex", "w") as f:
         f.write(
             generate_heat_sheet(
-                name="Sean Dutton Memorial Ocean Tendy Invitational",
+                name="Sean Dutton Memorial Ocean Tendie Invitational",
                 date="May 13, 2025",
                 location="Gesling Stadium",
                 host="Carnegie Mellon University",
                 events=events,
+                start_time=datetime.strptime("2025-05-13 18:00", "%Y-%m-%d %H:%M"),
+                between_event_time=timedelta(minutes=3),
             )
         )
     print("Heat sheet template generated as heat_sheet.tex")
